c3efdd195b3cc441c4681f5d82dd3c271dadffc6
Your Name <syd.ahad@gmail.com>
2026-01-30 20:33:01 +0530

    Onboarding Route created

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {getOnboardingStatus} from '@/actions/user'
+import { redirect } from 'next/navigation'
 
 const page = () => {
+  const {isOnboarded} = getOnboardingStatus()
+  if(!isOnboarded) {
+    redirect("/onboarding")
+  }
   return (
     <div className='text-white text-9xl p-50'>
       Dashboard Page
--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {getOnboardingStatus} from '@/actions/user'
+import { redirect } from 'next/navigation'
 
 const page = () => {
+  const {isOnboarded}= getOnboardingStatus()
+  if(isOnboarded){
+    redirect('/dashboard')
+  }
   return (
    <div className='text-white text-9xl p-50'>
       Onboarding Page
